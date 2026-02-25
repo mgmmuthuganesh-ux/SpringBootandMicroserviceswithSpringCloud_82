fbe14ece81c412a53f73226a93836786a5adbe78
HarshaVardhan <athaluri.harsha@gmail.com>
2026-02-25 09:07:43 +0530

    apigateway

---
+++ apigateway/orderservice/src/main/java/com/training/orderservice/controller/OrderController.java
@@ -0,0 +1,54 @@
+package com.training.orderservice.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.training.orderservice.dao.Order;
+import com.training.orderservice.dao.OrderRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RequestMapping("/order")
+@RestController
+public class OrderController {
+
+	@Autowired
+	OrderRepository repo;
+	
+	@GetMapping("/greet")
+	public String greet(@RequestHeader("service_name") String reqheader) {
+		System.out.println("service requested "+reqheader);
+		return "Greetings from OrderService Instance TWO ..........";
+	}
+	
+	@GetMapping("/uid/{uid}")
+	public List<Order> getOrderByUid(@RequestHeader("service_name") String reqheader,@PathVariable Integer uid){
+		return repo.findByUid(uid);
+	}
+	
+	
+	@PostMapping("/save")
+	public Order saveOrder(@RequestBody Order ord) {
+		return repo.save(ord);
+	}
+	
+	@GetMapping("/{oid}")
+	public Order getOrder(@PathVariable Integer oid) {
+		return repo.findById(oid)
+				.orElseThrow(()->new RuntimeException("Order not found "+oid));
+	}
+	
+	@GetMapping("/all")
+	public List<Order> getAllOrders(){
+		return (List<Order>) repo.findAll();
+	}
+	
+}
---
+++ apigateway/orderservice/src/main/java/com/training/orderservice/dao/Order.java
@@ -0,0 +1,46 @@
+package com.training.orderservice.dao;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "orders")
+@Entity
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer oid;
+	private String type;
+	private String status;
+	private Integer uid;
+	
+	public Integer getOid() {
+		return oid;
+	}
+	public void setOid(Integer oid) {
+		this.oid = oid;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Integer getUid() {
+		return uid;
+	}
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	
+	
+}

2c72e83b041080313e708d00000386d79c3e46be
HarshaVardhan <athaluri.harsha@gmail.com>
2026-03-12 08:54:21 +0530

    oauth2 token generator

---
+++ springsecurityOauth2/src/main/java/com/training/springsecurityOauth2/controllers/UserController.java
@@ -0,0 +1,32 @@
+package com.training.springsecurityOauth2.controllers;
+
+import java.util.List;
+
+import javax.annotation.Generated;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.training.springsecurityOauth2.dao.UserRepository;
+import com.training.springsecurityOauth2.dao.Users;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	UserRepository userepo;
+	
+	@GetMapping("/users")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public ResponseEntity<List<Users>> findAllusers(){
+		return new ResponseEntity<List<Users>>((List<Users>) userepo.findAll(),HttpStatus.OK);
+	}
+	
+	
+}
